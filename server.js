140e593f957f0784f8a77ba9188b74f52f705595
Nikita45500 <caskibog@gmail.com>
2026-05-12 14:20:11 +0300

    fix render port

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
-
 app.use(express.static("webapp"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/webapp/index.html");
 });
+
+// 🔥 ВАЖНО ДЛЯ RENDER
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
