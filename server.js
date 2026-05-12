f61ae7bb972d8cd57769f926cd21cb14a5b583ce
Nikita45500 <caskibog@gmail.com>
2026-05-12 14:44:45 +0300

    debug server minimal http

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("SERVER IS WORKING");
-});
+const http = require("http");
 
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server started:", PORT);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK SERVER WORKING");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on", PORT);
 });
