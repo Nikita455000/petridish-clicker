8777cfaa696ed9d404cb03eaeb1e57846b64e063
Nikita45500 <caskibog@gmail.com>
2026-05-12 14:41:46 +0300

    test minimal server

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-// 📁 фронтенд
-app.use(express.static(path.join(__dirname, "webapp")));
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "webapp", "index.html"));
+  res.send("SERVER IS WORKING");
 });
 
-// 🔥 FIX PORT (ОЧЕНЬ ВАЖНО)
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port:", PORT);
+  console.log("Server started:", PORT);
 });
