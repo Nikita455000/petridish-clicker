90caa47cb8b48d4c7aa7325fa103b3ab293baca4
Nikita45500 <caskibog@gmail.com>
2026-05-12 14:52:52 +0300

    fix render minimal server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 10000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("OK SERVER WORKING");
+  res.end("OK WORKING");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
